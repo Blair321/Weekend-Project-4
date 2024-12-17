1d0f1a0207c03d60112998b814f77eaaaa933e0b
Blair Jilek <blairjilek@gmail.com>
2024-12-17 12:59:13 -0600

    got my get call to work

--- src/components/GalleryList/GalleryList.jsx
+++ src/components/GalleryList/GalleryList.jsx
@@ -1,6 +1,27 @@
+
+import { useState, useEffect } from "react";
+import axios from "axios";
 function GalleryList() {
+  const [galleryItems, setGalleryItems] = useState([]);
+
+useEffect(()=>{
+  fetchGalleryItems();
+},[]);
+
+   function fetchGalleryItems() {
+    axios.get('/api/gallery').then (function(response){
+      console.log('back from GET:', response.data);
+      setGalleryItems(response.data);
+    }).catch(function(err){
+      console.log(err);
+      alert('error getting my galleryItems');
+      
+    })
+   }
   
-    return (
+  
+  
+  return (
       <div>
           <h1>GalleryList</h1>
       </div>
